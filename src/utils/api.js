86da9f6bd08c01a2a00f6d035da76dc0c29fc670
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2024-01-27 18:57:59 +0530

    initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL='https://api.themoviedb.org/3';
+
+
+const headers={
+
+
+    Authorization:'Bearer'+process.env.REACT_APP_TMDB_TOKEN,
+};
+
+export const fetchDataFromAPI =async (url,params)=>{
+try {
+    const {data}=await axios.get(BASE_URL+url,{
+       headers,
+       params
+    })
+    return data;
+} catch (error) {
+    console.log(error);
+    return error;
+}
+}
